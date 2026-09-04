a9e2d7f9971838bff2209030088c6fe41b14aa94
bakaboyplaystoomuchstarrail-dotcom <bakaboyplaystoomuchstarrail@gmail.com>
2026-09-05 06:43:38 +0800

    ALMOST working model. Precision is off and sometimes dupes happen even when no new buildings can be bought. Also doesn't account for second building type omittance

--- main.py
+++ main.py
@@ -24,13 +24,17 @@ class BuildingType():
         return(self.BulkBuyFormula(self.AmountOwned))
 
     def Calc_BuyCost(self,_NumBuildings=1):#Accounts for scenarios in which buildings are alr owned
-        return(self.BulkBuyFormula(_NumBuildings)-self.Value) 
+        return(self.BulkBuyFormula(self.AmountOwned + _NumBuildings)-self.Value) 
 
     def Calc_SellPrice(self,_NumBuildings):
         return(self.Value - self.BulkBuyFormula(self.AmountOwned-_NumBuildings))
 
     def Calc_MaxBuyable(self,_Gap):
-            return(math.floor(math.log((_Gap*0.15/self.BasePrice)+1,1.15)))
+        MaxBuyable = 0
+        while self.Calc_BuyCost(MaxBuyable+1) < _Gap:
+            MaxBuyable += 1
+        return(MaxBuyable)
+        # return(math.floor(math.log((_Gap*0.15/(self.BasePrice*((1.15**self.AmountOwned)-1)))+1,1.15)))
 
     def copy(self):
         # Return a new BuildingType with the same attribute values
@@ -44,7 +48,6 @@ Mine = BuildingType("Mine",1200,47)
 Factory = BuildingType("Factory",13000,260)
 BuildingTypes = [Cursor,Grandma,Farm,Mine,Factory] #These are all of the types that exist within the game, most of them have a base price above the goal amount though and as such I use "BuildingTypesWithinScope" for those instead
 
-CombIDCounter = 0
 class Combination():
     def __init__(self,_ID):
         self.ID = _ID
@@ -64,24 +67,24 @@ class Combination():
     def Gap(self):
         return(Goal - self.CookiesBaked - 0.0001) #fah If the cost of a purchase would put the sum total purchases cost to the same level as the goal, then there's no point considering it since the very frame the purchase becomes unlockable the simulation would've ended.
     
-    def AddPurchase(self,_BuildingType,_PurchaseAmount):
-        self.CookiesBaked += _BuildingType.Calc_BuyCost(_PurchaseAmount)
-        _BuildingType.AmountOwned += _PurchaseAmount
+    def AddPurchase(self,_BuildingTypeID,_PurchaseAmount):
+        self.CookiesBaked += self.BuildingTypesWithinScope[_BuildingTypeID].Calc_BuyCost(_PurchaseAmount)
+        self.BuildingTypesWithinScope[_BuildingTypeID].AmountOwned += _PurchaseAmount
 
-    def AddMaxBuildingType(self,_BuildingType):
-        self.AddPurchase(_BuildingType,_BuildingType.Calc_MaxBuyable(self.Gap))
+    def AddMaxBuildingType(self,_BuildingTypeID):
+        self.AddPurchase(_BuildingTypeID,self.BuildingTypesWithinScope[_BuildingTypeID].Calc_MaxBuyable(self.Gap))
 
     def AddMaxPurchases(self):
         Counter = 0
         while self.Gap > self.BuildingTypesWithinScope[Counter].BasePrice:#shouldn't be base price but it doesn't matter because this func is only ever called to find corners, meaning no buildings will be owned 
-            self.AddMaxBuildingType(self.BuildingTypesWithinScope[Counter])
+            self.AddMaxBuildingType(Counter)
             Counter += 1
             if Counter == len(self.BuildingTypesWithinScope):
                 break
     
-    def OmitPurchase(self,_BuildingType,_OmittedAmt):
-        self.CookiesBaked -= _BuildingType.Calc_SellPrice(_OmittedAmt)
-        _BuildingType.AmountOwned -= _OmittedAmt
+    def OmitPurchase(self,_BuildingTypeID,_OmittedAmt):
+        self.CookiesBaked -= self.BuildingTypesWithinScope[_BuildingTypeID].Calc_SellPrice(_OmittedAmt)
+        self.BuildingTypesWithinScope[_BuildingTypeID].AmountOwned -= _OmittedAmt
 
     def PrintDetails(self):
         print(self.CookiesBaked)
@@ -91,33 +94,28 @@ class Combination():
         print("===============================")
 
 
-    def Dupe(self,_NewCombID=CombIDCounter):
-        New_Comb = Combination(_NewCombID)
+    def Dupe(self):
+        New_Comb = Combination(self.ID + 1)
         New_Comb.CookiesBaked = self.CookiesBaked
-        New_Comb.AllocationList = self.AllocationList
-        New_Comb.OmittedType = self.OmittedType
         New_Comb.BuildingTypesWithinScope = []
         for x in range(len(self.BuildingTypesWithinScope)):
-            New_Comb.BuildingTypesWithinScope[x] = self.BuildingTypesWithinScope[x].copy()
+            New_Comb.BuildingTypesWithinScope.append(self.BuildingTypesWithinScope[x].copy())
         return(New_Comb)
 
-RootCombination = Combination(CombIDCounter) 
+RootCombination = Combination(0) 
 RootCombination.AddMaxPurchases() #830 iis the sweeet spot or smth
 BuildingPurchasesCombinations = [RootCombination] #The array containing all the combinations of building purchases affordable within the scope of the goal
 
 DuplicateCount = 0
 Curr_Comb = BuildingPurchasesCombinations[0]
 for w in range(len(Curr_Comb.BuildingTypesWithinScope)):
-    Curr_Comb.OmittedType = Curr_Comb.BuildingTypesWithinScope[w]
-    Curr_Comb.AllocationList = Curr_Comb.BuildingTypesWithinScope
-    Curr_Comb.AllocationList.pop(w)
-    for x in range(1,Curr_Comb.OmittedType.AmountOwned):#omitting amt
-        for i in range(len(Curr_Comb.AllocationList)):#allocating to everyone who hasn't been omitted
-            if Curr_Comb.OmittedType.Calc_SellPrice(x) > Curr_Comb.AllocationList[i].Calc_BuyCost():
+    other_indices = [idx for idx in range(len(Curr_Comb.BuildingTypesWithinScope)) if idx != w]
+    for x in range(1, Curr_Comb.BuildingTypesWithinScope[w].AmountOwned):
+        for orig_idx in other_indices:
+            if Curr_Comb.BuildingTypesWithinScope[w].Calc_SellPrice(x) > Curr_Comb.BuildingTypesWithinScope[orig_idx].Calc_BuyCost():
                 New_Comb = Curr_Comb.Dupe()
-                CombIDCounter += 1
-                New_Comb.OmitPurchase(New_Comb.OmittedType,x)
-                New_Comb.AddMaxBuildingType(New_Comb.AllocationList[i])
+                New_Comb.OmitPurchase(w, x)
+                New_Comb.AddMaxBuildingType(orig_idx)   # now correct
                 BuildingPurchasesCombinations.append(New_Comb)
                 New_Comb.PrintDetails()
                 
